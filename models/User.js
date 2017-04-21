21b766e7c864ca861d269adfe07636643e2d1e0f
Kit Langton <kitlangton@gmail.com>
2017-04-21 13:26:12 -0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -13,14 +13,9 @@ UserSchema.methods.validatePassword = function(password) {
   return bcrypt.compareSync(password, this.passwordHash);
 };
 
-UserSchema.virtual("password")
-  .get(function() {
-    return this._password;
-  })
-  .set(function(value) {
-    this._password = value;
-    this.passwordHash = bcrypt.hashSync(value, 8);
-  });
+UserSchema.virtual("password").set(function(value) {
+  this.passwordHash = bcrypt.hashSync(value, 8);
+});
 
 const User = mongoose.model("User", UserSchema);
 
