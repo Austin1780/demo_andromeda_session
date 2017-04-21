db7ee54cb67c08b0c8ab4e155c906454a9dc7f9b
Kit Langton <kitlangton@gmail.com>
2017-04-21 16:40:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
+// Create our app
 const app = require("express")();
+
+// Require and mount the cookie and body parser middlewares
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Connect to our mongo server
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/test");
-const db = mongoose.connection;
 
+// Set up express-handlebars
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Require our User model and Session helpers
 const User = require("./models/User");
 const {
   createSignedSessionId,
@@ -13,41 +27,17 @@ const {
   loggedOutOnly
 } = require("./services/Session");
 
-const exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+// Mount our custom loginMiddleware
 app.use(loginMiddleware);
 
+// Home route
 app.get("/", loggedInOnly, (req, res) => {
   res.render("home");
 });
 
-app.get("/logout", (req, res) => {
-  res.cookie("sessionId", "");
-  res.redirect("/");
-});
-
+// Login routes
 app.get("/login", loggedOutOnly, (req, res) => {
   res.render("login");
-});
-
-app.get("/register", loggedOutOnly, (req, res) => {
-  res.render("register");
-});
-
-app.post("/register", (req, res) => {
-  const { email, password } = req.body;
-  const newUser = new User({ email, password });
-  newUser.save((err, user) => {
-    if (err) return res.send("ERROR");
-
-    const sessionId = createSignedSessionId(email);
-    res.cookie("sessionId", sessionId);
-    res.redirect("/");
-  });
 });
 
 app.post("/login", (req, res) => {
@@ -65,4 +55,28 @@ app.post("/login", (req, res) => {
   });
 });
 
+// Registration Routes
+app.get("/register", loggedOutOnly, (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const { email, password } = req.body;
+  const newUser = new User({ email, password });
+  newUser.save((err, user) => {
+    if (err) return res.send("ERROR");
+
+    const sessionId = createSignedSessionId(email);
+    res.cookie("sessionId", sessionId);
+    res.redirect("/");
+  });
+});
+
+// Logout route
+app.get("/logout", (req, res) => {
+  res.cookie("sessionId", "");
+  res.redirect("/");
+});
+
+// Start our app
 app.listen(2000);
